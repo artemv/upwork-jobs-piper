5f8e043f0e2795e525ef55638fec3970a85eb264
Artem Vasiliev <artem.job@gmail.com>
2016-02-15 23:39:11 +0300

    test: corrected name

--- spec/requests/job_posts_spec.rb
+++ spec/requests/job_posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'JobPosts', type: :request do
   describe 'GET /job_posts' do
-    it 'works! (now write some real specs)' do
+    it 'works with empty data' do
       get job_posts_path
       expect(response).to have_http_status(200)
     end
