6dd7b2d12c121a890387e817214985c30ef0df08
Artem Vasiliev <artem.job@gmail.com>
2016-02-24 16:44:11 +0300

    test: JobFilters::HourlyRateFilter

---
+++ spec/lib/job_filters/hourly_rate_filter_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe JobFilters::HourlyRateFilter do
+  let(:valid_job_data) { {'title' => 'ruby'} }
+
+  let(:job_data) { valid_job_data.merge(spec_job_data) }
+
+  describe "should pass when it's fine" do
+    let(:spec_job_data) { {'title' => 'great rails project'} }
+    subject { JobFilters::HourlyRateFilter.good_job?(job_data) }
+    it { is_expected.to be true }
+  end
+
+  describe "should not pass when it has no whitelisted substring" do
+    let(:spec_job_data) { {'title' => 'great project'} }
+    subject { JobFilters::HourlyRateFilter.good_job?(job_data) }
+    it { is_expected.to be false }
+  end
+
+end
