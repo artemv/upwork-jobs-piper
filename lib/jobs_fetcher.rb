4a1af197f5009c87b16d6c2316ac358d3b6d258c
Artem Vasiliev <artem.job@gmail.com>
2016-02-29 12:31:37 +0300

    style: minor formatting

--- lib/jobs_fetcher.rb
+++ lib/jobs_fetcher.rb
@@ -31,7 +31,6 @@ class JobsFetcher
   end
 
   private
-  
 
   def process_page(page_idx, jobs, search)
     reporter = ->(delta, result) { "#{delta}s spent by Upwork API to find #{result.size} jobs on page #{page_idx}" }
