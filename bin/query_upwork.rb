5dbd647cfb437d2fa8bc7a94b253f10de374f7fd
Artem Vasiliev <artem.job@gmail.com>
2016-04-29 00:33:13 +0300

    feat: a JSON Upwork API querier utility

---
+++ bin/query_upwork.rb
@@ -0,0 +1,26 @@
+#!/Users/artem/.rvm/rubies/ruby-2.2.2/bin/ruby
+require "dotenv"
+require_relative '../lib/upwork_client_setup'
+require 'upwork/api/routers/jobs/profile'
+require 'upwork/api/routers/freelancers/profile'
+
+Dotenv.load
+
+client = UpworkClientSetup.setup_client
+result = nil
+if ENV['JOB_ID']
+  profile = Upwork::Api::Routers::Jobs::Profile.new(client)
+  result = profile.get_specific(ENV['JOB_ID'])
+elsif ENV['FREELANCER_ID']
+    profile = Upwork::Api::Routers::Freelancers::Profile.new(client)
+    result = profile.get_specific(ENV['FREELANCER_ID'])
+else
+  variants = []
+  indent = "  "
+  variants << 'JOB_ID="_~01a9a3a697ce5e4cf5" ruby bin/query_upwork.rb'
+  variants << 'FREELANCER_ID="~01af205f17cc6d5f2d" ruby bin/query_upwork.rb'
+  puts "Usage:\n#{indent}" + variants.join(" OR\n#{indent}")
+end
+
+fail unless result
+puts JSON.pretty_generate(result)
